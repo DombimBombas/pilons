c07e42ed8c1562117b9d4ba659a4889db023bbc2
DombimBombas <ilya_balakin@list.ru>
2024-06-23 20:07:48 +0500

    23.06 20:07

--- pilons/App.cs
+++ pilons/App.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Autodesk.Revit.UI;
+using System;
 using System.Collections.Generic;
+using System.Configuration.Assemblies;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace pilons
 {
-    public class App
+    public class App : IExternalApplication
     {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //вкладка
+            string tabName = "Вкладка-хуядка";
+            application.CreateRibbonTab(tabName);
+
+            //панель на вкладке
+            RibbonPanel panel = application.CreateRibbonPanel(tabName, "ТИУ рулит");
+
+            //кнопка на панели
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location,
+                   iconsDirectoryPath = Path.GetDirectoryName(assemblyLocation) + @"\icons\";
+            PushButtonData buttonData = new PushButtonData("btnSplitColumns", "Split Columns", assemblyLocation, "ColumnSplitter.BreakColumnsCommand");
+            panel.AddItem(buttonData);
+
+            return Result.Succeeded;
+
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+
     }
 }
