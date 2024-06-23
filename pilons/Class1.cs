81ca919362b8327deeb6b38b0162ea23f90a2456
ilya <ilya@ILYA>
2024-06-23 19:51:08 +0500

    Добавьте файлы проекта.

---
+++ pilons/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pilons
+{
+    public class Class1
+    {
+    }
+}
